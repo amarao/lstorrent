255e64c759c48d366975e87c82358902853d9e5a
george.shuklin <george.shuklin@5624b7bc-d7f4-11de-b0e0-6bdc6d9bcfd0>
2009-11-27 00:37:35 +0000

    first working step - q-n-d processing

--- functions.c
+++ functions.c
@@ -36,31 +36,41 @@ item_t* get_record(item_t* root,const unsigned char* name){
 }
 
 
-typedef enum {
-    skip,
-    filename_only,
-    filename_and_paths
-}options_t;
-
-typedef options_t (*pass_filter_callback_t(item_t* item));
-/*right now return 0/1 to filter files*/
 
 void print_file_data(item_t* f){
+    item_t* length=get_record(f,"length"); /*right now has no meaning*/
+    item_t* path=get_record(f,"path");
+    item_t* name=get_record(f,"name");
+    int c;
+    if (name){
+        printf("%s\n",name->str);     
+        return;
+    }
+    if (path){
+        for(c=0;c<path->list->count;c++){
+            if(c)
+                printf("/");
+            printf("%s",path->list->array[c].str);
+        }
+        printf("\n");
+    }
+
 }
 
-int process_filelist(item_t* root, pass_filter_callback_t *filter){
+int process_filelist(item_t* root, int *filter(item_t* )){
     item_t* info=get_record(root,"info");
     item_t* filelist=get_record(info,"files");
     int c;
     if (!info)
         return 0;
     /*we have to cases: 'info' contains an files record for mutlifile torrent or contain data about file for single-file torrent*/
-    if(filelist){
-        if(filter(info)){
+    if(!filelist){
+    /*first case - single-file torrent*/
+        if(filter(info)){/*TODO something with dir-only mode filtering*/
             print_file_data(info);
         }
-    /*first case - mulitfile torrent*/
-    }else{ /*second case - single-file torrent*/
+    /*second case - mulitfile torrent*/
+    }else{ 
         for(c=0;c<filelist->list->count;c++){
             if(filter(filelist->list->array+c)){
                 print_file_data(filelist->list->array+c);
@@ -68,4 +78,6 @@ int process_filelist(item_t* root, pass_filter_callback_t *filter){
         }
     }
 }
+
+
 
--- functions.h
+++ functions.h
@@ -1,1 +1,11 @@
 item_t* get_record(item_t* root,const unsigned char* name);
+
+typedef enum {
+    skip,
+    filename_only,
+    filename_and_paths
+}options_t;
+
+typedef options_t (*pass_filter_callback_t(item_t* item));
+/*right now return 0/1 to filter files*/
+
