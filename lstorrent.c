949cd5847fb0cbb29224a046bc2b1e083b70cb8a
george.shuklin <george.shuklin@5624b7bc-d7f4-11de-b0e0-6bdc6d9bcfd0>
2009-11-29 00:48:36 +0000

    minor fixes to main routinue

--- lstorrent.c
+++ lstorrent.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
 #include "types.h"
 #include "bdecode.h"
 #include "functions.h"
@@ -17,29 +19,52 @@
 
 
 int main(int argc, char* argv[]){
-#define MAX_FILE 10*1024*1024
-/*Note: I understand, than this is lame, I make normal reading later, when deside, how to read it (by single fread or by streamed reading on-demand, like media files*/
-    unsigned char* buf=malloc(MAX_FILE);
-    int size=0;
+#define MAX_FILE_SIZE 15*1024*1024
+/*I think, this is resonally big for *.torrent file. If you wish, you can try to use bigger values, but note, that parsing of files requied about 3 times of buffer size memory*/
+
+    unsigned char* buf;
+    off_t size=0;
+    off_t fsize=0;
     FILE* f;
     item_t * res;
+    struct stat st;    
     int c;
     if (argc<2){
         puts(err_help);
         exit(0);
     }
     for(c=1;c<argc;c++){
+        if(stat(argv[c],&st)){
+            printf(err_nostat,argv[c],strerror(errno));
+            continue;
+        };
+        fsize=st.st_size;
+        if(!fsize){
+            printf(err_empty,argv[c]);
+            continue;
+        }
+        if(fsize<0||fsize>MAX_FILE_SIZE){
+            printf(err_torrent_too_big,argv[0],(intmax_t)fsize);
+            continue;
+        }
+        buf=malloc(fsize+1);
+        if(!buf){
+            printf(err_nomem);
+            continue;
+        }
         f=fopen(argv[c],"rb");
         if(!f){
             printf("%s\n",strerror(errno));
-            return -1;
+            continue;
         }
-        size=fread(buf,1,MAX_FILE,f);
-        if (size==MAX_FILE)
-            oops(err_torrent_too_big);
+        size=fread(buf,1,fsize,f);
+        if (size!=fsize)
+            printf(err_noread,argv[c],strerror(errno));
         fclose(f);
+        buf[fsize]=0;
         res=bdecode(buf,size);   
         debug(res);
+        /*process here*/
     }
     return 0;
 }
--- ui.c
+++ ui.c
@@ -8,7 +8,10 @@ const char* err_nomem="not enough memory";
 const char* err_bad="bad file format";
 const char* err_internal="internal error (bad call or bad parameters)";
 const char* err_help="(here must be help)";
-const char* err_torrent_too_big="Torrent is too big";
+const char* err_torrent_too_big="File %s is too big (%d), skipping\n";
+const char* err_nostat="stat() error for file %s: %s\n";
+const char* err_empty="File %s is empty, skipping.\n";
+const char* err_noread="Unable to read %s: %s\n";
 
 void oops_message(const char* message,int line){
     printf ("%d: %s\n",line,message);
